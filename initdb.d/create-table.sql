deded1f9f89a30286de11d7ac55552e4fabcfeca
SHIVA_YUYA <nikyaku405@gmail.com>
2024-07-10 14:39:23 +0900

    dockerが起動しないバグの修正-タイプミス

--- initdb.d/create-table.sql
+++ initdb.d/create-table.sql
@@ -1,3 +1,11 @@
 -- Windows環境で文字化け対応
 SET CHARACTER SET utf8mb4;
 
+create table library (
+    book_id int auto_increment primary key,
+    book_name varchar(255) not null,
+    book_num int not null,
+    book_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+insert into library (book_name,book_num) values ('hoge','1',);

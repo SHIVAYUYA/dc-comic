48c6611fefff963dfc015cd4a87c4c1edb82b2c1
SHIVA_YUYA <nikyaku405@gmail.com>
2024-07-13 19:08:22 +0900

    ReactのheaderComponentの作成

---
+++ app/src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export const Header = function() {
+    return <header>
+        <div className="header__inner">
+            <h1 className="header_title">PORTABLE BOOK CONTAINER</h1>
+        </div>
+    </header>
+}
